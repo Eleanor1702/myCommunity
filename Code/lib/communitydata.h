c7d38ac054b5305d44535750d1172f5a8c6d990f
Stephi Mennle <stephimennle@gmail.com>
2018-05-15 15:55:59 +0200

    database

---
+++ Code/lib/communitydata.h
@@ -0,0 +1,38 @@
+#ifndef COMMUNITYDATA_H
+#define COMMUNITYDATA_H
+
+#include <vector>
+#include <string>
+#include <cppconn/connection.h>
+#include <cppconn/prepared_statement.h>
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <mysql_driver.h>
+#include <mysql_connection.h>
+#include "resident.h"
+#include "room.h"
+
+class CommunityData {
+private:
+    sql::mysql::MySQL_Driver *driver;
+    sql::Connection* con;
+    sql::ConnectOptionsMap connection_properties;
+
+public:
+    bool connect();
+    void createRoomTable();
+    void createResidentTable();
+    void addResident(string, int);
+    void addRoom(string, string);
+    void updatePassword(string, int);
+    void deleteResident(string);
+    void deleteRoom(string);
+    vector<Resident> getAllResidents();
+    vector<Room> getAllRooms();
+    CommunityData();
+    ~CommunityData();
+};
+
+#endif // COMMUNITYDATA_H
