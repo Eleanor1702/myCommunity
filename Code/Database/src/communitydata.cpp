182c2596c307d4b0bd93ef145a09214bc77dcb93
Noor Alrabea <eleanor.alrabea@gmail.com>
2018-05-16 14:33:08 +0200

    edit

--- Code/Database/src/communitydata.cpp
+++ Code/Database/src/communitydata.cpp
@@ -1,4 +1,4 @@
-#include "Database/lib/communitydata.h"
+/*#include "Database/lib/communitydata.h"
 
 using namespace std;
 using namespace sql;
@@ -135,5 +135,4 @@ CommunityData* CommunityData::getInstance() {
     }
     return instance;
 }
-
-
+*/
