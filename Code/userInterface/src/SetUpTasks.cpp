efbfcd252ba726bc7d4aa14e676e3b6d665e8b5b
Noor Alrabea <eleanor.alrabea@gmail.com>
2018-05-29 16:23:41 +0200

    Rooms und Tasks appear function bearbeitet

--- Code/userInterface/src/SetUpTasks.cpp
+++ Code/userInterface/src/SetUpTasks.cpp
@@ -118,9 +118,7 @@ std::string SetUpTasks::getTaskNameInput() {
 }
 
 void SetUpTasks::appear(std::vector<std::string> nameVec, std::vector<std::string> roomVec, std::vector<std::string> frequencyVec, int size) {
-    this->show();
-
-    for(unsigned int i = 0; i < TaskListItemList.size(); i++) {
+    for(int i = 0; i < TaskListItemList.size(); i++) {
         scrollLayout->removeWidget(TaskListItemList[i]);
     }
 
@@ -137,6 +135,8 @@ void SetUpTasks::appear(std::vector<std::string> nameVec, std::vector<std::strin
     }
 
     giveNameEdit->clear();
+
+    this->show();
 }
 
 
